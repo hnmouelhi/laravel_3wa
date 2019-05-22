a580ed91b202c2c0e696d63e68ebf420b03ae052
hnmouelhi <hnmouelhi@gmail.com>
2019-05-22 14:37:18 +0100

    first commit

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -2,5 +2,6 @@
 		<ul>
 		<li><span style="color:red;"> student name :</span>{{$students->name}} </li>
 		<li><span style="color:red;"> email :</span>{{$students->email}}</li>
+		<li><span style="color:red;"> classroom_id :</span>{{$students->classroom_id}}</li>
 		<li><span style="color:red;">title classroom :</span>{{$students->classroom->title}}</li> 
         </ul>
